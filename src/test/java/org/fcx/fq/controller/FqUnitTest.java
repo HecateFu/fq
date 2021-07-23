89d487794bb4fe551f4ce2d89af836c22046c1b3
HecateFu <hecatefu@outlook.com>
2021-07-23 23:50:25 +0800

    增加ss解析格式

--- src/test/java/org/fcx/fq/controller/FqUnitTest.java
+++ src/test/java/org/fcx/fq/controller/FqUnitTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 
 import java.time.LocalTime;
 
-public class FqUnitTes {
+public class FqUnitTest {
     @Test
     public void trojanTest() {
-        String l0 = "ss://YWVzLTI1Ni1nY206eHBRd3lWNFc1RmRBNk5NQU5KSng3M1VTQDIuNTguMjQxLjI3OjM4MDMz/#shadowsocks%2F2.58.241.27%3A38033-iwcqe\r";
+        String l0 = "ss://cmM0LW1kNTpwYXNzZncyeHM0ZSE@117.28.243.187:8333#@ShareCentre%20%F0%9F%87%A8%F0%9F%87%B3%20CN-%F0%9F%87%B9%F0%9F%87%BCTW_2776%20%EF%BC%88%E5%89%AF%E6%9C%AC%EF%BC%89";
         Proxy proxy0 = Proxy.factory(l0);
         System.out.println(proxy0);
     }
