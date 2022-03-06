4b1f85bc66f722af60d0428ba528e7ff26dc0916
HecateFu <hecatefu@outlook.com>
2022-03-06 12:08:59 +0800

    增加test case

--- src/test/java/org/fcx/fq/controller/FqControllerTest.java
+++ src/test/java/org/fcx/fq/controller/FqControllerTest.java
@@ -1,6 +1,7 @@
 package org.fcx.fq.controller;
 
 import org.fcx.fq.entity.proxy.clash.Proxy;
+import org.fcx.fq.util.MyToolUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,16 +34,29 @@ public class FqControllerTest {
         mvc.perform(get("/fq/config/ml.yaml")).andExpect(status().isOk());
     }
     @Test
-    public void decodeServerInfoTest() throws Exception{
-        String link = "https://sub.qaq.wtf/";
+    public void decodeServerInfoTest() {
+        String link = "https://raw.githubusercontent.com/ssrsub/ssr/master/ssrsub";
         String raw = fq.downloadProxyServersRaw(link);
         Set<Proxy> proxies = fq.parseProxyList(raw);
-        proxies.stream().forEach(System.out::println);
+        proxies.forEach(System.out::println);
+    }
+
+    @Test
+    public void createProxyTest(){
+        Proxy proxy = Proxy.factory("ssr://MTAxLjY3LjguMTkyOjI1MTc5OmF1dGhfYWVzMTI4X21kNTphZXMtMjU2LWNmYjp0bHMxLjJfdGlja2V0X2F1dGg6YUVkclVUWTVNVFYwUkEvP29iZnNwYXJhbT1ZV3BoZUM1dGFXTnliM052Wm5RdVkyOXQmcHJvdG9wYXJhbT1Nekl6T0RFNlF6SndTSGxwVVdabmJRJnJlbWFya3M9UTA1ZjVMcU01NGkzNTcrNzVhS1o1NzJSSUdoMGRIQnpPaTh2TVRnd09DNW5ZVjh4TkRNPSZncm91cD02YnVZNks2azVZaUc1N3VF");
+        System.out.println(proxy);
+    }
+
+    @Test
+    public void base64DecodeTest(){
+        String result = MyToolUtil.base64Decode("Q05f5LqM54i357+75aKZ572RIGh0dHBzOi8vMTgwOC5nYV8xNDM=");
+        System.out.println(result);
     }
 
     @Test
     public void updateLinksTest () throws  Exception{
         String resp = mvc.perform(MockMvcRequestBuilders
+
                 .get("/fq/updatelink")
                 .param("key","ss")
                 .param("link","https%3A%2F%2Fwww.111003.ml%2F%2Flink%2FXp7XUEOqe3dYQVNu%3Fsub%3D1"))
